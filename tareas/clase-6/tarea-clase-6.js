9638a7e1d3494d738277de1825b726be0e487b84
Kund097 <facund097dv@gmail.com>
2022-09-22 13:02:53 -0300

    Tarea 1 clase 6

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -6,11 +6,150 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+const $botonEnviarFamiliares = document.querySelector('#enviar');
 
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+$botonEnviarFamiliares.onclick = function() {
+    const $cantidadFamiliares = Number(document.querySelector('#grupo-familiar').value);
+    if ($cantidadFamiliares > 0) {
+        mostrarBotonCalcular(true);
+    }
+    detectarInputsABorrar();
+    crearElementos($cantidadFamiliares);
+}
 
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
+const $botonLimpiarFormulario = document.querySelector('#limpiar');
+
+$botonLimpiarFormulario.onclick = function() {
+    limpiarInputs();
+    mostrarBotonCalcular(false);
+    mostrarResultados(false);
+}
+
+const $botonCalcular = document.querySelector('#calcular');
+
+$botonCalcular.onclick = function() {
+    obtenerEdades();
+    calcularMayorEdad(obtenerEdades());
+    calcularMenorEdad(obtenerEdades());
+    calcularPromedioEdad(obtenerEdades());
+    mostrarResultados(true);
+}
+
+
+function crearElementos(familiares) {
+
+    for (let i = 0; i < familiares; i++) {
+
+        const $div = document.createElement('div');
+        $div.className = "familiar";
+
+        const $label = document.createElement('label');
+        $label.textContent = `Familiar Nº ${i + 1}  `;
+
+        const $input = document.createElement('input');
+        $input.className = "edad";
+        $input.setAttribute("type","number");
+
+        document.querySelector('form').appendChild($div);
+        $div.appendChild($label);
+        $div.appendChild($input);
+    }
+}
+
+function mostrarBotonCalcular(trueOrFalse) {
+    const $displayCalcular = document.querySelector('#calcular');
+    if (trueOrFalse) {
+        $displayCalcular.style.display = "inline"
+    } else {
+        $displayCalcular.style.display = "none"
+    }
+    
+}
+
+function limpiarInputs() {
+    const $nodoPadre = document.querySelector('form');
+    const $familiares = document.querySelectorAll('.familiar');
+    for (let i = 0; i < $familiares.length; i++) {
+        $nodoPadre.removeChild($familiares[i]);
+    }
+}
+
+function detectarInputsABorrar() {
+    const $familiaresIngresados = document.querySelector('#grupo-familiar').value;
+    const $inputABorrar = document.querySelectorAll('.familiar');
+    if ($familiaresIngresados != 0) {
+        if ($inputABorrar.length != "") {
+            limpiarInputs();
+        }
+    }
+}
+
+function obtenerEdades() {
+    const $edadesFamiliares = document.querySelectorAll('.edad');
+    const ARRAY_EDADES = [];
+    for (let i = 0; i < $edadesFamiliares.length; i++) {
+        if ($edadesFamiliares[i].value != 0) {
+            ARRAY_EDADES.push(Number($edadesFamiliares[i].value));
+        } 
+        
+    }
+    return ARRAY_EDADES;
+}
+
+function calcularMayorEdad(array) {
+    let mayor = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (mayor < array[i]) {
+            mayor = array[i]
+        } 
+    }
+    textoResultados(mayor,1);
+    return console.log(`El mayor es: ${mayor}`);
+}
+
+function calcularMenorEdad(array) {
+    let primerElemento = array[0];
+    let menor = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (primerElemento < array[i]) {
+            menor = primerElemento;
+        } else {
+            menor = array[i];
+            primerElemento = array[i]
+        }
+    }
+    textoResultados(menor,2);
+    return console.log(`El menor es: ${menor}`);
+}
+
+function calcularPromedioEdad(array) {
+    let promedio = 0;
+    let sumaTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumaTotal += array[i];
+    }
+    promedio = (sumaTotal / array.length).toFixed(2);
+    textoResultados(promedio,3);
+    return console.log(`El promedio es: ${promedio}`);
+}
+
+function textoResultados(resultado,texto) {
+    if (texto === 1) {
+        document.querySelector('#mayor').textContent = `El mayor es: ${resultado}`;
+    }
+    if (texto === 2) {
+        document.querySelector('#menor').textContent = `El menor es: ${resultado}`
+    }
+    if (texto === 3) {
+        document.querySelector('#promedio').textContent = `El promedio es: ${resultado}`
+    }
+}
+
+function mostrarResultados(trueOrFalse) {
+    $resultados = document.querySelector('#resultado');
+    if (trueOrFalse) {
+        $resultados.style.display = "inline"
+    } else {
+        $resultados.style.display = "none"
+    }
+}
